da9c895ed3c5786c1012719433d2ca7e2c430fc8
AgneseBussone <agnese.bussone@teoresigroup.com>
2017-03-16 14:57:39 -0400

    lap listview

--- app/src/main/java/watch/stopwatch/MessageHandler.java
+++ app/src/main/java/watch/stopwatch/MessageHandler.java
@@ -1,9 +1,11 @@
 package watch.stopwatch;
 
+import android.content.Context;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -16,15 +18,17 @@ public class MessageHandler extends Handler {
 
     // Messages available
     public static final int MSG_STOPWATCH_START     = 0;
-    public static final int MSG_STOPWATCH_STOP      = 1;
+    public static final int MSG_STOPWATCH_RESET     = 1;
     public static final int MSG_STOPWATCH_UPDATE    = 2;
     public static final int MSG_STOPWATCH_PAUSE     = 3;
     public static final int MSG_STOPWATCH_RESUME    = 4;
     public static final int MSG_STOPWATCH_LAP       = 5;
     public static final int MSG_STOPWATCH_SAVE_LAP  = 6;
+    public static final int MSG_STOPWATCH_SHOW_LAP  = 7;
 
     private Chronometer timer = new Chronometer();
     private final long REFRESH_RATE = 100;
+    private Context context;
 
     // stopwatch graphical asset
     private TextView stopwatch_tv = null;
@@ -33,10 +37,13 @@ public class MessageHandler extends Handler {
 
     // time info for stopwatch laps
     private long stopwatch_start;
-    private ArrayList<Long> laps;
+    private long last_lap;
+    private ArrayList<String[]> laps = new ArrayList<>();
+    private MyArrayAdapter lapsListAdapter;
 
-    public MessageHandler(Looper looper){
+    public MessageHandler(Looper looper, Context context){
         super(looper);
+        this.context = context;
     }
 
     void initStopwatch(TextView time_tv, ImageView img, TextView btn_tv){
@@ -64,13 +71,18 @@ public class MessageHandler extends Handler {
                 sendEmptyMessageDelayed(MSG_STOPWATCH_UPDATE,REFRESH_RATE);
                 break;
 
-            case MSG_STOPWATCH_STOP:
+            case MSG_STOPWATCH_RESET:
                 removeMessages(MSG_STOPWATCH_UPDATE); // no more updates.
                 timer.stop();//stop timer
                 if(stopwatch_needle != null & stopwatchBtn_tv != null) {
                     stopwatch_tv.setText(R.string.time_default);
-                    stopwatch_needle.setRotation(0f);
+                    stopwatch_needle.animate().rotation(0f);
                     stopwatchBtn_tv.setText(R.string.central_btn_start);
+                }
+                laps.clear();
+                if(lapsListAdapter != null) {
+                    lapsListAdapter.notifyDataSetChanged();
+                    lapsListAdapter = null;
                 }
                 break;
 
@@ -93,7 +105,26 @@ public class MessageHandler extends Handler {
 
             case MSG_STOPWATCH_LAP:
                 // add the time to the list
-                laps.add(timer.getElapsedTime());
+                long time = timer.getElapsedTime();
+                Time absolute = new Time(time);
+                if(laps.isEmpty()){
+                    // first entry: the two format are the same
+                    laps.add(new String[]{absolute.getFormattedTime(), absolute.getFormattedTime()});
+                }
+                else{
+                    laps.add(new String[]{absolute.getFormattedTime(), "relative - TBC"});
+                }
+                last_lap = time;
+                if(lapsListAdapter!= null)
+                    lapsListAdapter.notifyDataSetChanged();
+                break;
+
+            case MSG_STOPWATCH_SHOW_LAP:
+                if(msg.obj != null) {
+                    ListView list = (ListView)msg.obj;
+                    lapsListAdapter = new MyArrayAdapter(context, laps);
+                    list.setAdapter(lapsListAdapter);
+                }
                 break;
 
             case MSG_STOPWATCH_SAVE_LAP:
